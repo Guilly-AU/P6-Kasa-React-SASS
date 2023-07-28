f37a4223170c81c0b3492bbbeea49ff2cb25d8be
Guilly-AU <125476287+Guilly-AU@users.noreply.github.com>
2023-07-28 13:01:34 +0800

    hashrouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About'
-import Accomodation from './pages/Accomodation';
-import Error from './pages/Error';
-import "./scss/layout.scss"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  HashRouter,
+} from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Accomodation from "./pages/Accomodation";
+import Error from "./pages/Error";
+import "./scss/layout.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<Router>
-			<Header />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/about' element={<About />} />
-				<Route path='/logement/:id' element={<Accomodation />} />
-				<Route path='*' element={<Error />} />
-			</Routes>
-			<Footer />
-		</Router>
-	</React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/logement/:id" element={<Accomodation />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </HashRouter>
+  </React.StrictMode>
 );
