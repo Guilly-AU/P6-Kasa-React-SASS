af5838a8401a9af1f455115c97716887280d3827
Guilly-AU <125476287+Guilly-AU@users.noreply.github.com>
2023-07-28 12:06:21 +0800

    update

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,44 +1,53 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import ArrowUp from "../assets/arrow-up.png";
-import "../scss/collapse.scss"
+import "../scss/collapse.scss";
 
 function Collapse({ title, description }) {
-   const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-   const handleToggle = () => {
+  const handleToggle = () => {
     setIsOpen(!isOpen);
-   };
+  };
 
-   const contentRef = useRef();
+  const contentRef = useRef();
 
   return (
-   <article className="collapse-container">
+    <article className="collapse-container">
       <div className="title-wrapper">
-         <h3 className="collapse-title-texte" key={title}>{title}</h3>
-         <div onClick={handleToggle}>
-            <img 
-            className={isOpen ? "arrow-rotate down" : "arrow-rotate"} 
-            src={ArrowUp} 
-            alt={isOpen ? "Flèche vers le bas." : "Flèche vers le haut."}/>
-         </div>
+        <h3 className="collapse-title-texte" key={title}>
+          {title}
+        </h3>
+        <div onClick={handleToggle}>
+          <img
+            className={isOpen ? "arrow-rotate down" : "arrow-rotate"}
+            src={ArrowUp}
+            alt={isOpen ? "Flèche vers le bas." : "Flèche vers le haut."}
+          />
+        </div>
       </div>
-      <div className="content-parent"
-         ref={contentRef} 
-         style={isOpen ? { height: contentRef.current.scrollHeight + "px" } : { height: "0px" }}>
-            <div className="description-wrapper">
-            {Array.isArray(description) ? (
-               <ul>
-                  {description.map((item, index) => (
-                  <li key={index}>{item}</li>
-                  ))}
-               </ul>
-            ) : ( 
-               <p>{description}</p>
-            )}
-            </div>
+      <div
+        className="content-parent"
+        ref={contentRef}
+        style={
+          isOpen
+            ? { height: contentRef.current.scrollHeight + "px" }
+            : { height: "0px" }
+        }
+      >
+        <div className="description-wrapper">
+          {Array.isArray(description) ? (
+            <ul>
+              {description.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>{description}</p>
+          )}
+        </div>
       </div>
-   </article>
-  )
+    </article>
+  );
 }
 
 export default Collapse;
